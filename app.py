2491ccf414b867ca8114076f7f80d8ef42897048
Anurag Parasar <anurag.mundd@gmail.com>
2026-05-27 01:25:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import cv2
 from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
 import av
 from huggingface_hub import hf_hub_download
-from tensorflow.keras.models import load_model
 
 model_path = hf_hub_download(
     repo_id="Anurag04/Fer_model",
